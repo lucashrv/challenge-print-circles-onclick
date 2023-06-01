05de5a4d76cfa9b857a546b3746ef03f7284a1b9
lucashrv <lucas.hrv@outlook.com>
2023-06-01 03:30:36 -0300

    challenge-print-circles-onclick

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [coordinates, setCoordinates] = useState([])
+
+  const getCoodinates = (e) => {
+    const { clientX, clientY } = e
+
+    setCoordinates([ ...coordinates, { clientX, clientY }])
+    console.log(e);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={getCoodinates}>
+      {coordinates.map((coords, i) => {
+        return (
+          <div
+            key={i}
+            className='circle'
+            style={{ left: coords.clientX - 25, top: coords.clientY - 25, zIndex: i }}
+          ></div>)
+      })}
     </div>
   );
 }
